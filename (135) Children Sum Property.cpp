dad2e724e0426d3f5cd1e3fd720a83a87514054d
Sri Ganesh <sriganeshiiits@gmail.com>
2023-07-05 11:13:45 +0530

    solved the 135th question.

---
+++ (135) Children Sum Property.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+    if(root==0) return ;
+
+    int child = 0;
+
+    if(root->left) child+=root->left->data;
+    if(root->right) child+=root->right->data;
+
+    if(root->data<=child){
+        root->data = child;
+    }
+    else{
+        if(root->left) root->left->data = root->data;
+        if(root->right) root->right->data = root->data;
+    }
+
+    changeTree(root->left);
+    changeTree(root->right);
+
+    int total = 0;
+    if(root->left) total += root->left->data;
+    if(root->right) total += root->right->data;
+    if(total) root->data = total;
+
+}  
